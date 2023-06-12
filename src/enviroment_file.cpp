fba6588477b04c7ebf514cc514b3059f67318cd7
Callum <Callum@cantonim.me>
2023-06-12 23:23:19 +0100

    adding some tests to the enviroment file

--- src/enviroment_file.cpp
+++ src/enviroment_file.cpp
@@ -1,6 +1,9 @@
 
 #ifdef TEST_ENVIROMENT_FILE
 #include "test_utils.hpp"
+#include <filesystem>
+
+#define TEST_FILE "test.txt"
 #endif
 #include "enviroment_file.hpp"
 
@@ -23,12 +26,19 @@ EnviromentFile::EnviromentFile(string file_name){
 
 #ifdef TEST_ENVIROMENT_FILE
 TEST(constructor, a test to ensure the default constructor works) {
-    TEST_ASSERT(true,Figuring out if this works at all);
-    TEST_ASSERT(true,Figuring out if this works at all);
-    TEST_ASSERT(true,Figuring out if this works at all);
-    TEST_ASSERT(true,Figuring out if this works at all);
-    TEST_ASSERT(true,Figuring out if this works at all);
-    TEST_ASSERT(true,Figuring out if this works at all);
+    //Writing out a test file to disk so that if a file gets read
+    //There will be values in the EnviromentFile object
+    std::ofstream test_file(TEST_FILE);
+    test_file << "TEST=TEST" << std::endl;
+    test_file.close();
+
+    EnviromentFile file(TEST_FILE);
+
+    TEST_ASSERT(file.file_name() == TEST_FILE,ensuring that the file name is set properly during the constructor);
+    TEST_ASSERT(file.get("TEST") == std::nullopt,ensuring that no file has been read.);
+
+    //clean up after ourself
+    TEST_ASSERT(std::filesystem::remove(TEST_FILE),Failed to remove test file);
 }
 #endif
 
@@ -43,42 +53,84 @@ string EnviromentFile::file_name(){
 void EnviromentFile::read(){
     ifstream file;
     file.exceptions(ifstream::badbit);
+    
+    file.open(_file_name);
 
-    try {
+    if (!file) throw NoSuchFileException();
+    
+    string line;
+    
+    while (std::getline(file,line))
+    {
+        size_t split_posistion = line.find('=');
 
-        file.open(_file_name);
-        string line;
-        
-        while (std::getline(file,line))
-        {
-            size_t split_posistion = line.find('=');
+        if (split_posistion == string::npos) {
+            throw BadFileFormatException();
+        }
 
-            if (split_posistion == string::npos) {
-                std::cerr << "ERROR: " << _file_name << "is in a invalid file format" << std::endl;
-                exit(-1);
-            }
+        string key = line.substr(0,split_posistion);
+        string value = line.substr(split_posistion+1);
+        _data.insert(std::make_pair(key,value));
 
-            string key = line.substr(0,split_posistion);
-            string value = line.substr(split_posistion+1);
-            _data.insert(std::make_pair(key,value));
-
-        } 
-    } catch(const ifstream::failure& e) {
-
-        std::cerr << "ERROR: " << e.what() << std::endl;
-        exit(-1);
-
-    }
+    } 
 }
 
 #ifdef TEST_ENVIROMENT_FILE
-TEST(read, a test for the read function) {
-    TEST_ASSERT(true,Figuring out if this works at all);
-    TEST_ASSERT(true,Figuring out if this works at all);
-    TEST_ASSERT(true,Figuring out if this works at all);
-    TEST_ASSERT(true,Figuring out if this works at all);
-    TEST_ASSERT(true,Figuring out if this works at all);
-    TEST_ASSERT(true,Figuring out if this works at all);
+TEST(unread_get, a test ensuring that pre read and pre set that the enviroment file is clean) 
+{
+    EnviromentFile file(TEST_FILE);
+    
+    TEST_ASSERT(file.get("TEST") == std::nullopt, when no file has been read that nullopt is returned);
+}
+
+TEST(file_not_exist,testing that if a file does not exist on disk that we thrown an exception)
+{
+    
+    EnviromentFile file(TEST_FILE);
+    
+    try {
+        file.read();
+        TEST_ASSERT(false, testing an exception is thrown when a file is not on disk);
+    }catch(...) {
+        TEST_ASSERT(true, testing that when a file does exsist in the correct format is read correctly);
+    }
+
+}
+
+TEST(normal_file_read, testing if a file is in the correct format it is read){
+
+    std::ofstream test_file(TEST_FILE);
+    test_file << "TEST=TEST" << std::endl;
+    test_file.close();
+
+    EnviromentFile file(TEST_FILE);
+    
+    try {
+        file.read();
+        TEST_ASSERT(file.get("TEST") == std::optional<std::string>{std::string("TEST")}, testing that the value in memory correctly reflects what is on disk);
+    }catch(...) {
+        TEST_ASSERT(false, testing that if a file of a correct format exists no exception is thrown);
+    }
+
+    TEST_ASSERT(std::filesystem::remove(TEST_FILE),Failed to remove test file);
+}
+
+TEST(invalid_file_read, testing if a file is not in the correct format it is read){
+
+    std::ofstream test_file(TEST_FILE);
+    test_file << "TEST TEST" << std::endl;
+    test_file.close();
+
+    EnviromentFile file(TEST_FILE);
+    
+    try {
+        file.read();
+        TEST_ASSERT(false, testing that if a file of a incorrect format exists an exception is thrown);
+    }catch(...) {
+        TEST_ASSERT(true, testing that if a file of a incorrect format exists an exception is thrown);
+    }
+
+    TEST_ASSERT(std::filesystem::remove(TEST_FILE),Failed to remove test file);
 }
 #endif
 
@@ -120,6 +172,17 @@ optional<std::string> EnviromentFile::get(std::string key){
     return optional<string>{position->second};
 }
 
+#ifdef TEST_ENVIROMENT_FILE
+TEST(get_set, a test to prove the basic of the get and set functonality ) {
+    TEST_ASSERT(true,Figuring out if this works at all);
+    TEST_ASSERT(true,Figuring out if this works at all);
+    TEST_ASSERT(true,Figuring out if this works at all);
+    TEST_ASSERT(true,Figuring out if this works at all);
+    TEST_ASSERT(true,Figuring out if this works at all);
+    TEST_ASSERT(true,Figuring out if this works at all);
+}
+#endif
+
 void EnviromentFile::set(std::string key, optional<std::string> value){
     _data.insert(std::make_pair(key,value.value_or(std::string())));
 }
@@ -132,6 +195,17 @@ void EnviromentFile::append(map<std::string,std::string> data, bool overwrite){
         }
     }
 }
+
+#ifdef TEST_ENVIROMENT_FILE
+TEST(append, append functonality) {
+    TEST_ASSERT(true,Figuring out if this works at all);
+    TEST_ASSERT(true,Figuring out if this works at all);
+    TEST_ASSERT(true,Figuring out if this works at all);
+    TEST_ASSERT(true,Figuring out if this works at all);
+    TEST_ASSERT(true,Figuring out if this works at all);
+    TEST_ASSERT(true,Figuring out if this works at all);
+}
+#endif
 
 EnviromentFile::iterator EnviromentFile::begin(){
     return _data.begin();
